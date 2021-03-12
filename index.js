a3e1c4abbd8df2d8132eec2f40a7a71c96817b70
Ximengao123 <62191675+CuteCatXiao@users.noreply.github.com>
2021-03-12 21:52:11 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -17,26 +17,6 @@ var browser = {
     language: (navigator.browserLanguage || navigator.language).toLowerCase()
 }
 if (!browser.versions.mobile) {//判断是否是移动设备打开。browser代码在下面
-    //上吊的猫
-    $(function () {
-        //scroll 事件适用于所有可滚动的元素和 window 对象（浏览器窗口）。
-        $(window).scroll(function () {
-            var scroHei = $(window).scrollTop();//滚动的高度
-            if (scroHei > 500) {
-                $('.back-to-top').css('top', '-200px');
-                // $('.back-to-top').fadeIn();
-            } else {
-                $('.back-to-top').css('top', '-999px');
-                // $('.back-to-top').fadeOut();
-            }
-        })
-        /*点击返回顶部*/
-        $('.back-to-top').click(function () {
-            $('body,html').animate({
-                scrollTop: 0
-            }, 600);
-        })
-    })   
 
     document.createElement("script").setAttribute("src", "https://cdn.jsdelivr.net/gh/CuteCatXiao/cdn_cpcs@main/js/szgotop.js");
 
